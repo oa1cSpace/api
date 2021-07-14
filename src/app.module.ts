ff7cddf842795e45db7587bb91c265cceb33a7c9
oa1cspace <oa1cspace@gmail.com>
2021-07-14 20:59:37 +0300

    feat: added Mongo config and addition packages + .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageController } from './top-page/top-page.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
   controllers: [AppController, TopPageController],
   providers: [AppService],
-  imports: [ProductModule, AuthModule, ReviewModule],
+  imports: [
+      ConfigModule.forRoot(),
+      TypegooseModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: getMongoConfig
+      }),
+      ProductModule, AuthModule, ReviewModule],
 })
 export class AppModule { }
