cb318052a4b6b7fce43f6d23479b57480cf6d76d
oa1cspace <oa1cspace@gmail.com>
2021-10-10 14:48:38 +0300

    minor-fix: add PORT to .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3333;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.info(`\n\n\n===>    APP STARTED ON: http:localhost:${PORT}/api/\n\n\n`);
 }
 bootstrap();
